5044dde2de54864d8b9cd35c1b8ed621ba10bb54
parthdandekar2467 <77429360+parthdandekar2467@users.noreply.github.com>
2021-07-24 18:22:52 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,30 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    fill("white");
+    textSize(15);
+    text("U C T O N N F I", 600, 150);
+    fill("lightgreen");
+
+    text("Hint: We create different ________ for different pc/npc !!", 600, 170);
+
+    fill("white");
+    textSize(15);
+    text("A T A D A S B A", 200, 290);
+    fill("yellow");
+
+    text("Hint: Real time Database !!", 200, 310)
+    
+
+}
